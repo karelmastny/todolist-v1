1d00e0045a38bc784a2904a9526b42d42f536716
Karel Mastny <karelgithub@gmail.com>
2021-02-13 21:37:15 +0100

    ejs and date module added

--- app.js
+++ app.js
@@ -1,65 +1,58 @@
-
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
+const items = ["First task"];
+const workItems = [];
+
 
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 const port = process.env.PORT;
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
+    let day = date.getDate();
 
-
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-    switch (currentDay) {
-        case 1:
-            day = "Monday";
-            break;
-
-        case 2:
-            day = "Tuesday";
-            break;
-
-        case 3:
-            day = "Wednesday";
-            break;
-
-        case 4:
-            day = "Thursday";
-            break;
-
-        case 5:
-            day = "Friday";
-            break;
-
-        case 6:
-            day = "Saturday";
-            break;
-
-        case 0:
-            day = "Sunday";
-            break;
-        default:
-            console.log("Error: currentDay is equal to: " + currentDay);
-    }
-
-    res.render("list", {kindOfDay: day});
-
-
+    res.render("list", {
+        listTitle: day,
+        newListItems: items
+    });
 
 });
 
+app.post("/", function (req, res) {
+
+    let newTask = req.body.newItem;
+
+    if (req.body.list === "Work") {
+        workItems.push(newTask);
+        res.redirect("/work");
+    } else {
+        items.push(newTask);
+        res.redirect("/");
+    }
+
+})
 
 
+app.get("/work", function (req, res) {
+    res.render("list", {
+        listTitle: "Work List",
+        newListItems: workItems
+    });
+})
 
 
-app.listen(port || 3000, function(){
+app.get("/about", function (req, res) {
+    res.render("about");
+})
+
+
+app.listen(port || 3000, function () {
     console.log("Server started on port 3000.");
 });
